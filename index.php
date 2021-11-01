bdc5ac03882778988bcc6fc6372b243d123e5170
victorseraphin <victorseraphin@gmail.com>
2021-11-01 14:38:55 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <!--No campo "Título da página", digite o título da sua página-->
+        <title>SALED</title>
+    </head>
+    <body>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <table align="center" border="0">
+            <tr>
+                <!--No campo "meubanner.jpg" digite o nome da sua imagem e sua extensão (jpg, gif, png, etc..)
+                caso a imagem esteja dentro de uma subpasta em sua hospedagem, digite entre as aspas"", o nome da subpasta, exemplo: src="../subpasta/meubanner.jpg"-->
+                <td align="center"><img class="img-contrucao" src="./assets/img/site-em-construcao.png" ></td>
+            </tr>
+        </table>
+    </body>
+</html>
